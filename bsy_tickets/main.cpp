2483c80d318fa79e37b1d803ac827e4f1969347d
Marco Schöttelkotte <marcoschoettelkotte@gmx.de>
2022-05-05 18:28:57 +0200

    Initial Commit

---
+++ bsy_tickets/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  bsy_tickets
+//
+//  Created by Marco Schöttelkotte on 05.05.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
